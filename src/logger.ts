598da2b060bd5a3001374b0c11c467a57221fb3f
Aerilym <aerilymbiz@gmail.com>
2022-08-19 01:18:40 +1000

    Add custom winston-axios package

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,7 @@
 import { createLogger, transports, format } from 'winston';
 import Transport from 'winston-transport';
 import axios from 'axios';
+import { AxiosTransport } from 'winston-axios';
 
 import {
   LoggerConfig,
@@ -45,62 +46,6 @@ class ExternalLogConfig implements LoggerConfig {
       options: OptionsConfig;
     };
   };
-}
-
-interface AxiosTransportOptions extends Transport.TransportStreamOptions {
-  host: string;
-  auth: string;
-  path: string;
-  headers?: object;
-  batch?: boolean;
-  batchInterval?: number;
-  batchCount?: number;
-  replacer?: (key: string, value: any) => any;
-}
-
-//
-// Inherit from `winston-transport` so you can take advantage
-// of the base functionality and `.exceptions.handle()`.
-//
-class AxiosTransport extends Transport {
-  host: string;
-  auth: string;
-  path: string;
-
-  constructor(opts: AxiosTransportOptions) {
-    super(opts);
-
-    //
-    // Consume any custom options here. e.g.:
-    // - Connection information for databases
-    // - Authentication information for APIs (e.g. loggly, papertrail,
-    //   logentries, etc.).
-    //
-    this.host = opts.host;
-    this.auth = opts.auth;
-    this.path = opts.path;
-  }
-
-  log(info: any, callback: () => void) {
-    setImmediate(() => {
-      this.emit('logged', info);
-    });
-
-    axios
-      .post(this.host + this.path, info, {
-        headers: { Authorization: 'Bearer ' + this.auth },
-      })
-      .then(function (response) {
-        return response;
-      })
-      .catch(function (error) {
-        return error;
-      });
-
-    // Perform the writing to the remote service
-
-    callback();
-  }
 }
 
 // Create a logger instance with custom settings
